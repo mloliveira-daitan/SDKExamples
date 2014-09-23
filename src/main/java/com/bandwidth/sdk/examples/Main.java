81189bd0e56864a20825b3264bd7b7174bd8a423
Steve Mitchell <smitchell@bandwidth.com>
2014-09-23 15:52:22 -0600

    structure for heroku push

--- src/main/java/com/bandwidth/sdk/examples/Main.java
+++ src/main/java/com/bandwidth/sdk/examples/Main.java
@@ -1,17 +1,22 @@
-package main.java.com.bandwidth.sdk.examples;
+package com.bandwidth.sdk.examples;
 
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.webapp.WebAppContext;
 
-
+/**
+ * 
+ * This class launches the web application in an embedded Jetty container.
+ * This is the entry point to your application. The Java command that is used for
+ * launching should fire this main method.
+ *
+ */
 public class Main 
 {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) throws Exception 
-	{
+    
+    /**
+     * @param args
+     */
+    public static void main(String[] args) throws Exception{
         String webappDirLocation = "src/main/webapp/";
         
         //The port that we should run on can be set into an environment variable
@@ -39,11 +44,6 @@ public class Main
         
         server.start();
         server.join();   
-
-	}
+    }
 
 }
-
-
-
-
